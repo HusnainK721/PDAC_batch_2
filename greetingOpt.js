cda2c947e9bb8de614d60c6a1b0b8c06b7fba06b
HusnainK721 <husnaink721@gmail.com>
2021-09-17 15:16:32 +0530

    Create greetingOpt.js

---
+++ greetingOpt.js
@@ -0,0 +1,20 @@
+const greetingOpt=(type)=>{
+    switch(type){
+        case 1:
+            console.log("good morninng")
+            break
+            case 2:
+                console.log("good afternoon")
+                break
+                case 3:
+                    console.log("good evening")
+                    break
+                    case 4:
+                        console.log("good night")
+                        break
+                        default:
+                            console.log("hello")
+
+    }
+}
+greetingOpt(6)

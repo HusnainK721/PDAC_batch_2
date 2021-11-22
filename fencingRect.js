42b6e4d29f04bd11ac097577d2d922f979f4c9d3
HusnainK721 <husnaink721@gmail.com>
2021-11-22 12:25:16 +0530

    Update fencingRect.js

--- fencingRect.js
+++ fencingRect.js
@@ -1,3 +1,6 @@
 const fencingRect=(r, numberoflayer, rateofwire)=>{
 const parameterofshape=3.14*r
 const fencingwirelength =parameterofshape * numberoflayer
+const costofwire= fencingwirelength* rateofwire;
+return costofwire  
+}
